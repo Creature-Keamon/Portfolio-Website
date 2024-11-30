a138054e4e0669c3bcdc94e1105796807934ec21
Creature-Keamon <Shuttlez4@outlook.com>
2024-11-30 15:32:24 +1300

    organised AND functional

---
+++ src/about/main.js
@@ -0,0 +1,61 @@
+import * as THREE from "three";
+
+//Setup Scene
+const scene = new THREE.Scene()
+
+// Camera setup
+const aspectRatio = window.innerWidth / window.innerHeight;
+const camera = new THREE.OrthographicCamera(
+  -aspectRatio, // left
+  aspectRatio, // right
+  1, // top
+  -1, // bottom
+  0.1, // near
+  1000 // far
+);
+camera.position.set(-1,-0.1,1.7)
+
+//Setup Renderer
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'), alpha: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+function getWidth() {
+  return parseInt(window.getComputedStyle(canvas).width);
+}
+
+function getHeight() {
+  return parseInt(window.getComputedStyle(canvas).height);
+}
+
+addEventListener("resize",() => {
+  camera.aspect = getWidth() / getHeight();
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  cube.scale.set(0.5,0.5,0.5);
+},false);
+
+// Create Cube
+const geometry = new THREE.BoxGeometry( 1, 1.2, 1 );
+//const geometry = new THREE.CylinderGeometry( 0.73, 0.73, 1.4, 8 ); 
+
+const material = new THREE.MeshStandardMaterial( { color: 0x2c38ab } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+//setup point and ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0,0,1);
+scene.add(ambientLight,pointLight);
+
+//cube movement
+cube.rotation.x +=0.2;
+function animate() {
+	cube.rotation.y += 0.01;
+	renderer.render( scene, camera );
+
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
+import {resolve} from 'path'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 export default defineConfig({
+
+  root,
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'index.html',
-        about: 'About.html',
-        projects: 'Projects.html',
+        main: resolve(root, 'index.html'),
+        about: resolve(root, 'about', 'index.html'),
+        projects: resolve(root, 'projects', 'index.html'),
+        ai: resolve(root, 'projects', 'AI', 'index.html'),
+        ammit: resolve(root, 'projects', 'Ammit', 'index.html'),
+        remake: resolve(root, 'projects', 'Re-Make', 'index.html'),
+        thissite: resolve(root, 'projects', 'This-Site', 'index.html'),
+        travellersring: resolve(root, 'projects', 'Travellers-Ring', 'index.html'),
       },
     },
   },
